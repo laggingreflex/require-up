97bc945a85c734b3f65aaed5bd84d501ca423c7f
laggingreflex <laggingreflex@gmail.com>
2016-11-08 08:05:30 +0530

    fix: throw non-MODULE_NOT_FOUND errors
    If there's an error in a file that's not actually a MODULE_NOT_FOUND error, i.e. the file itself has indeed been found but there was an error parsing it, like perhaps an undefined variable being used or a syntax error, then it must throw that error and not mislead the user into thinking that the module/file was somehow not found.

--- main.js
+++ main.js
@@ -7,16 +7,24 @@ function requireUp( requestedModule, {
   getResolvedPath = false,
   paths = adjustModulePaths( module.parent.paths )
 } = {} ) {
+  const errors = [];
   for ( dirname of paths ) try {
     const tryPath = resolve( dirname, requestedModule );
     const modulePath = require.resolve( tryPath );
     const module = require( modulePath );
-    if ( getResolvedPath )
+    if ( getResolvedPath ) {
       return modulePath;
-    else
+    } else {
       return module;
-  } catch ( error ) {}
-  const err = new Error( "Cannot find module '" + requestedModule + "'" );
-  err.code = 'MODULE_NOT_FOUND';
-  throw err;
+    }
+  } catch ( error ) {
+    errors.push( error );
+    if ( error.code !== 'MODULE_NOT_FOUND' ) {
+      throw error;
+    }
+  }
+  const error = new Error( "Cannot find module '" + requestedModule + "'" );
+  error.code = 'MODULE_NOT_FOUND';
+  error.errors = errors;
+  throw error;
 }
