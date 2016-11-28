7de11009fafc2d3a4475a031a972ae4652698a14
laggingreflex <laggingreflex@gmail.com>
2016-11-28 17:18:14 +0530

    remove index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-console.warn(`WARNING: You're using 'require-up' as a module. Due to NodeJS's module caching it'll only be able to require up modules from the first parent it was required from. It's recommended to use the 'require-up/register' method instead.`);
-
-module.exports = require('./main');
